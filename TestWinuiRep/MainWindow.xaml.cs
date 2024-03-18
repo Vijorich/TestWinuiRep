68d69b2954ddbd93dc437eb5e2826c314f1ec1a8
vijor <vijorich@mail.ru>
2024-03-19 01:18:26 +0700

    Cleanup

--- TestWinuiRep/MainWindow.xaml.cs
+++ TestWinuiRep/MainWindow.xaml.cs
@@ -26,41 +26,4 @@ public sealed partial class MainWindow : Window
         Version.Text = $"{SysInfoText}";
         Lang.Text = Windows.Globalization.ApplicationLanguages.Languages[0];
     }
-
-    public string architectureString(Windows.System.ProcessorArchitecture architecture)
-    {
-        return architecture switch
-        {
-            Windows.System.ProcessorArchitecture.X86 => "x86",
-            Windows.System.ProcessorArchitecture.Arm => "arm",
-            Windows.System.ProcessorArchitecture.X64 => "x64",
-            Windows.System.ProcessorArchitecture.Neutral => "neutral",
-            Windows.System.ProcessorArchitecture.Unknown => "unknown",
-            Windows.System.ProcessorArchitecture.Arm64 => "arm64",
-            Windows.System.ProcessorArchitecture.X86OnArm64 => "x86OnArm64",
-            _ => "???"
-        };
-    }
-
-    //public static string GetAppVersion()
-    //{
-    //    var package = Package.Current;
-    //    var packageId = package.Id;
-    //    var version = packageId.Version;
-
-    //    return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
-    //}
-
-    //public static string GetTestVersion()
-    //{
-    //    StringBuilder sb = new StringBuilder();
-    //    Windows.Management.Deployment.PackageManager packageManager = new Windows.Management.Deployment.PackageManager();
-    //    foreach (var pkg in packageManager.FindPackages())
-    //    {
-    //        var version = pkg.Id.Version;
-    //        Debug.WriteLine(string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision));
-    //        sb.AppendLine($"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}");
-    //    }
-    //    return sb.ToString();
-    //}
 }
